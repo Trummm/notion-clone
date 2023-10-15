82e44d929e137ffa7bf80cc02a8484a4a29624cd
Trummm <trungpro2000.tp2@gmail.com>
2023-10-15 10:06:00 +0700

    setting function

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 
 import './globals.css'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -45,6 +46,7 @@ export default function RootLayout({
             storageKey='jotion-theme-2'
           >
             <Toaster position='bottom-center' />
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
