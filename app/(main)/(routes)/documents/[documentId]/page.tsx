25d97571a754b4e5197e7f419f60a167c57ce5bd
Trummm <trungpro2000.tp2@gmail.com>
2023-10-16 09:58:13 +0700

    cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -7,7 +7,7 @@ import { useMemo } from 'react'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { Toolbar } from '@/components/toolbar'
-// import { Cover } from "@/components/cover";
+import { Cover } from '@/components/cover'
 import { Skeleton } from '@/components/ui/skeleton'
 
 interface DocumentIdPageProps {
@@ -54,7 +54,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className='pb-40'>
-      {/* <Cover url={document.coverImage} /> */}
+      <Cover url={document.coverImage} />
       <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
         <Toolbar initialData={document} />
         {/* <Editor onChange={onChange} initialContent={document.content} /> */}
